555eb6fb3434ef9f9fcd6f7cee0eab69082eace3
Sasha <alecsand.unit@gmail.com>
2025-03-24 23:10:45 +0200

    Добавил возможность создания жанров

---
+++ WebApplication4/Controllers/GenreController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GenreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var ganre = _context.Genre.ToList();
+            return View(ganre); 
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Genre model)
+        {
+            if (_context.Genre.Any(g => g.Name == model.Name)) 
+            { 
+                ViewBag.Error = "Такой жанр уже существует";
+                return View(model);
+            }
+
+            _context.Genre.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");   
+        }
+    }
+}
